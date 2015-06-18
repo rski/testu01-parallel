e54f63bbb1b993e655681d3250c376981338c41c
rski <rom.skiad@gmail.com>
2015-06-18 19:51:16 +0100

    generator of the python kind that yields the tests in the order to be run

---
+++ parallel/order.py
@@ -0,0 +1,26 @@
+__all__ = ["testGenerator"]
+
+from test_definitions import TEST_ORDER_DIR, TEST_SUITES, TEST_SLAVE_EXECUTABLE
+import os
+
+
+def testOrderFromFile(testSuite):
+    testOrder = []
+    testOrderFileName = os.path.join(TEST_ORDER_DIR, "{0}_order.txt".format(testSuite))
+    print(testOrderFileName)
+    with open(testOrderFileName) as testOrderFile:
+        for line in testOrderFile:
+            testOrder.append(int(line))
+
+    return testOrder
+
+def testGenerator(testSuite):
+    testOrder = testOrderFromFile(testSuite)
+    for testNumber in testOrder:
+        test = ["./" + TEST_SLAVE_EXECUTABLE, testNumber, TEST_SUITES[testSuite]]
+        yield test
+
+    while True:
+        yield None
+
+
