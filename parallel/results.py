2da220e829d1477164206084dbaea1ade4d51f7c
rski <rom.skiad@gmail.com>
2015-06-25 03:53:30 +0100

    wrote a wrapper that will call the appropriate result function. For now it converts the binary output to strings and prints the results

--- parallel/results.py
+++ parallel/results.py
@@ -1,8 +1,13 @@
 from test_definitions import SUITE_SIZE
 
+def handleResults(results):
+    stringResults = binToString(results)
+    printResults(stringResults)
+
 def createResultsArray(testSuite):
     results = [None] * SUITE_SIZE[testSuite]
     return results
+
 
 def extractTestResults(rawResults):
     #result = [ testName, testNumber, p_value, rawResult ]
@@ -13,6 +18,7 @@ def extractTestResults(rawResults):
 
     return results
 
+
 def interpretResults(results):
     for result in results:
         resultList = result.split("\n")
@@ -20,6 +26,14 @@ def interpretResults(results):
 
     return interpretedResults
 
+
 def printResults(results):
     for result in results:
         print(result)
+
+
+def binToString(results):
+    stringResults = []
+    for result in results:
+        stringResults.append(result.decode('utf-8'))
+    return stringResults
