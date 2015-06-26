0f11cd6b008caf9beea56ea7bca3e38da98aa36e
rski <rom.skiad@gmail.com>
2015-06-26 08:07:41 +0100

    correct size of Crush

--- parallel/test_definitions.py
+++ parallel/test_definitions.py
@@ -6,5 +6,5 @@ TEST_SUITES = {"smallCrush":"0",
 
 TEST_ORDER_DIR = "test_order"
 SUITE_SIZE = {"smallCrush":10,
-                "Crush": 12,
+                "Crush": 96,
                 "bigCrush":106}
